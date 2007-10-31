9c39d9dcc6beaa34536d2bc2343446ce0b29a7aa
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2007-10-31 21:39:58 +0000

    IncludeHelper.php added, cosmetic updates in View.php, better examples.

--- examples/hello-world/views/index.php
+++ examples/hello-world/views/index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php $include = Helper::import('IncludeHelper'); ?>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Hello, World from a View!</title>
+    <?php echo $include->javascript('scripts/alert.js'); ?>
 </head>
 <body>
     Hello, World!
