80d99b39f19353cc65aee2f9eecf374791dea2f3
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2008-04-06 20:35:53 +0000

    Fixes, updates and comments.

--- trunk/src/IncludeHelper.php
+++ trunk/src/IncludeHelper.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 $Id$
 
 Class: IncludeHelper
@@ -17,9 +17,9 @@ About: Author
 About: License
 
     This file is licensed under the MIT.
-*/
+ */
 class IncludeHelper {
-
+    
     protected $javascripts;
     protected $stylesheets;
 
@@ -29,24 +29,27 @@ class IncludeHelper {
     }
 
     private function location($src) {
-
+        
         $location = $src;
         $urlarray = parse_url($src);
-
+        
         if ((!isset($urlarray['scheme'])) && (strpos($src, '/') !== 0)) {
-            if ($path = trim(dirname($_SERVER['SCRIPT_NAME']), '\,/')) {
+            
+            $path = trim(dirname($_SERVER['SCRIPT_NAME']), '\,/');
+            
+            if (strlen($path) > 0) {
                 $path = '/' . $path . '/';
             } else {
                 $path = '/';
             }
-
+            
             $location = $path . $src;
         }
-
+        
         return $location;
     }
-    
-    /*
+
+    /**
     Function: javascript
     
 		Registers a javascript-file include.
@@ -62,13 +65,13 @@ class IncludeHelper {
 		> // We're assuming that you've imported the helper to $include
 		> echo $include->javascript('scripts/javascript.js');
 		> echo $include->javascript('scripts/pngfix.js', '<!--[if lt IE 7.]>', '<![endif]-->');
-    */
+     */
     public function javascript($src, $before = '', $after = '') {
         $this->javascripts[$src] = sprintf('%s<script type="text/javascript" src="%s"></script>%s', $before, self::location($src), $after);
         return $this->javascripts[$src];
     }
 
-    /*
+    /**
     Function: javascripts
     
 		Goes through all registred javascript-files.
@@ -79,21 +82,21 @@ class IncludeHelper {
 		> $include->javascript('scripts/javascript.js');
 		> $include->javascript('scripts/pngfix.js', '<!--[if lt IE 7.]>', '<![endif]-->');
 		> echo $include->javascripts();
-    */
+     */
     public function javascripts() {
-
+        
         $space = '';
-
+        
         foreach ($this->javascripts as $javascript) {
             echo $space, $javascript, "\n";
-
+            
             if (strlen($space) === 0) {
                 $space = '    ';
             }
         }
     }
 
-    /*
+    /**
     Function: stylesheet
     
 		Registers a css-file include.
@@ -111,13 +114,13 @@ class IncludeHelper {
 		> echo $include->stylesheet('css/screen.css');
 		> echo $include->stylesheet('css/ie.js', 'screen, projection', '<!--[if IE]>', '<![endif]-->');
 		> echo $include->stylesheet('css/print.css', 'print');
-    */
+     */
     public function stylesheet($src, $media = 'screen, projection', $before = '', $after = '') {
         $this->stylesheets[$src] = sprintf('%s<link rel="stylesheet" href="%s" type="text/css" media="%s" />%s', $before, self::location($src), $media, $after);
         return $this->stylesheets[$src];
     }
 
-    /*
+    /**
     Function: stylesheets
     
 		Goes through all registred css-files.
@@ -129,14 +132,14 @@ class IncludeHelper {
 		> $include->stylesheet('css/ie.js', 'screen, projection', '<!--[if IE]>', '<![endif]-->');
 		> $include->stylesheet('css/print.css', 'print');
 		> echo $include->stylesheets();
-    */
+     */
     public function stylesheets() {
-
+        
         $space = '';
-
+        
         foreach ($this->stylesheets as $stylesheet) {
             echo $space, $stylesheet, "\n";
-
+            
             if (strlen($space) === 0) {
                 $space = '    ';
             }
