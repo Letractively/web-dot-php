3e13fefceeb6682bfa41d4a343cfc2f2f0285798
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2009-03-01 23:10:28 +0000

    Changed Router.php to extend ArrayIterator rather than implementing ArrayAccess.

--- trunk/src/Router.php
+++ trunk/src/Router.php
@@ -1,25 +1,19 @@
 <?php
-class Router implements ArrayAccess {
-
-    private $routes;
+class Router extends ArrayIterator {
 
     public function __construct($routes = null) { $this->routes = (is_array($routes)) ? $routes : array(); }
-    public function offsetSet($pattern, $route) { $this->routes[$pattern] = $route; }
-    public function offsetExists($pattern) { return isset($this->routes[$pattern]); }
-    public function offsetUnset($pattern) { unset($this->routes[$pattern]); }
-    public function offsetGet($pattern) { return isset($this->routes[$pattern]) ? $this->routes[$pattern] : null; }
 
     public function add($pattern, $route = null) {
         if (is_array($pattern)) {
-            $this->routes = array_merge($this->routes, $pattern);
+            $this[] = array_merge($this, $pattern);
         } else {
-            $this->routes[$pattern] = $route;
+            $this[$pattern] = $route;
         }
     }
 
     public function route() {
         $url = trim(substr(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), strlen(substr($_SERVER['SCRIPT_NAME'], 0, -9))), '/');
-        foreach ($this->routes as $pattern => $route) {
+        foreach ($this as $pattern => $route) {
             if (!isset($route)) return array($pattern, null);
             $regex = '#^' . $pattern . '$#i';
             if (!preg_match($regex, $url, $args) > 0) continue;
