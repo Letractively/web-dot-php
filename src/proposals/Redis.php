23d2bd553688d3f51000d7a34610fe65ad263a8c
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2009-08-16 12:56:50 +0000

    Impemented more Redis commands.

--- src/proposals/Redis.php
+++ src/proposals/Redis.php
@@ -8,34 +8,67 @@ class Redis {
         $this->port = $port;
     }
 
+    // Connection handling
+    function quit() { return $this->write("QUIT\r\n", true); }
+    function auth($password) { return $this->write(sprintf("AUTH %s\r\n", $password)); }
+
+    // Connection testing
     function ping() { return $this->write("PING\r\n"); }
     function echoes($data) { return $this->write(sprintf("ECHO %u\r\n%s\r\n", strlen($data), $data)); }
-    function auth($password) { return $this->write(sprintf("AUTH %s\r\n", $password)); }
-    function type($key) { return $this->write(sprintf("TYPE %s\r\n", $key)); }
+
+    // Commands operating on string values
     function set($key, $value) { return $this->write(sprintf("SET %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
-    function setnx($key, $value) { return $this->write(sprintf("SETNX %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
     function get($key) { return $this->write(sprintf("GET %s\r\n", $key)); }
     function getset($key, $value) { return $this->write(sprintf("GETSET %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
     function mget() { return $this->write(sprintf("MGET %s\r\n", implode(' ', func_get_args()))); }
-    function del() { return $this->write(sprintf("DEL %s\r\n", implode(' ', func_get_args()))); }
+    function setnx($key, $value) { return $this->write(sprintf("SETNX %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
     function incr($key) { return $this->write(sprintf("INCR %s\r\n", $key)); }
     function incrby($key, $increment = 1) { return $this->write(sprintf("INCRBY %s %d\r\n", $key, $increment)); }
     function decr($key) { return $this->write(sprintf("DECR %s\r\n", $key)); }
     function decrby($key, $increment = 1) { return $this->write(sprintf("DECRBY %s %d\r\n", $key, $increment)); }
     function exists($key) { return $this->write(sprintf("EXISTS %s\r\n", $key)); }
-    function llen($key) { return $this->write(sprintf("LLEN %s\r\n", $key)); }
-    function lpush($key, $value) { return $this->write(sprintf("LPUSH %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
+    function del() { return $this->write(sprintf("DEL %s\r\n", implode(' ', func_get_args()))); }
+    function type($key) { return $this->write(sprintf("TYPE %s\r\n", $key)); }
+
+    // Commands operating on the key space
+    function keys($pattern) { return $this->write(sprintf("KEYS %s\r\n", $pattern)); }
+    function randomkey() { return $this->write("RANDOMKEY\r\n"); }
+    function rename($oldkey, $newkey) { return $this->write(sprintf("RENAME %s %s\r\n", $oldkey, $newkey)); }
+    function renamenx($oldkey, $newkey) { return $this->write(sprintf("RENAMENX %s %s\r\n", $oldkey, $newkey)); }
+    function dbsize() { return $this->write("DBSIZE\r\n"); }
+    function expire($key, $timeout = 0) { return $this->write(sprintf("EXPIRE %s %u\r\n", $key, $timeout)); }
+    function ttl($key) { return $this->write(sprintf("TTL %s\r\n", $key)); }
+
+    // Commands operating on lists *
     function rpush($key, $value) { return $this->write(sprintf("RPUSH %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
+    function lpush($key, $value) { return $this->write(sprintf("LPUSH %s %u\r\n%s\r\n", $key, strlen($value), $value)); }
+    function llen($key) { return $this->write(sprintf("LLEN %s\r\n", $key)); }
+    function lrange($key, $start = 0, $end = -1) { return $this->write(sprintf("LRANGE %s %d %d\r\n", $key, $start, $end)); }
+    function ltrim($key, $start = 0, $end = -1) { return $this->write(sprintf("LTRIM %s %d %d\r\n", $key, $start, $end)); }
+    function lindex($key, $index = 0) { return $this->write(sprintf("LINDEX %s %d\r\n", $key, $index)); }
+    function lset($key, $index, $value) { return $this->write(sprintf("LSET %s %d %u\r\n%s\r\n", $key, $index, strlen($value), $value)); }
+
     function lpop($key) { return $this->write(sprintf("LPOP %s\r\n", $key)); }
     function rpop($key) { return $this->write(sprintf("RPOP %s\r\n", $key)); }
-    function lrange($key, $start = 0, $end = -1) { return $this->write(sprintf("LRANGE %s %u %d\r\n", $key, $start, $end)); }
+
+    // Commands operating on sets *
+
+
+    // Multiple databases handling commands
+    function select($dbindex = 0) { return $this->write(sprintf("SELECT %u\r\n", $dbindex)); }
+    function move($key, $dbindex) { return $this->write(sprintf("MOVE %s %u\r\n", $key, $dbindex)); }
+    function flushdb() { return $this->write("FLUSHDB\r\n"); }
+    function flushall() { return $this->write("FLUSHALL\r\n"); }
+
+    // Sorting *
+
+    // Persistence control commands
     function save() { return $this->write("SAVE\r\n"); }
     function bgsave() { return $this->write("BGSAVE\r\n"); }
     function lastsave() { return $this->write("LASTSAVE\r\n"); }
-    function flushdb() { return $this->write("FLUSHDB\r\n"); }
-    function flushall() { return $this->write("FLUSHALL\r\n"); }
-    function quit() { return $this->write("QUIT\r\n", true); }
     function shutdown() { return $this->write("SHUTDOWN\r\n", true); }
+
+    // Remote server control commands *
 
     private function write($command, $disconnect = false) {
         if (!isset($this->socket)) $this->socket = fsockopen($this->host, $this->port);
@@ -44,6 +77,10 @@ class Redis {
     }
 
     private function read() {
+
+        //$data = fgets($this->socket);
+        //$type = substr($data, 0, 1);
+        //$data = substr($data, 1, -2);
 
         $type = fgetc($this->socket);
         $data = substr(fgets($this->socket), 0, -2);
