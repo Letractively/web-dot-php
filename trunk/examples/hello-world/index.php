a82c66e688f910a22fa17e38d46b401f343cfa2d
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2009-08-21 07:33:53 +0000

    Modified example, and fixed routing.

--- trunk/examples/hello-world/index.php
+++ trunk/examples/hello-world/index.php
@@ -34,23 +34,24 @@ error_reporting(E_ALL | E_STRICT);
  * Dispatch Request
  * ======================================================================= */
 
-get('footer.html');
+get('/', function () {
 
-get('/:action/:param', function ($action, $param) {
-    
     $form = new Form();
-    $form->email->filters = array('email', '/^aapo/');
     $form->email = 'aapo.laakkonen@gmail.com';
 
     $view = new View('views/index.phtml');
-    $view->header = 'Lorem Ipsum ' . $action . ' ' . $param;
+    $view->header = 'Lorem Ipsum';
     $view->body = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     $view->form = $form;
 
     echo $view;
 });
 
-route('IndexController->notfound');
-//run();
+post('/', function () {
+    $form = new Form($_POST);
+    echo '<pre>';
+    print_r($form);
+    echo '</pre>';
+});
 
-//web\run('404.php');
+route('IndexController->notfound');
