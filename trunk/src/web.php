e2553ee23c1e8bbe1407191578289f3224126fc6
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-02-11 01:27:56 +0000

    Updated block implementation, added refactored url function, added slug function.

--- trunk/src/web.php
+++ trunk/src/web.php
@@ -14,16 +14,37 @@ About: License
 
     This file is licensed under the MIT.
 */
-function get($path, $func = null) { return ($_SERVER['REQUEST_METHOD'] == 'GET') ? route($path, $func) : false; }
-function post($path, $func = null) { return ($_SERVER['REQUEST_METHOD'] == 'POST') ? route($path, $func) : false; }
-function put($path, $func = null) { return ($_SERVER['REQUEST_METHOD'] == 'PUT' || (isset($_POST['_method']) && strcasecmp($_POST['_method'], 'PUT') == 0)) ? route($path, $func) : false; }
-function delete($path, $func = null) { return ($_SERVER['REQUEST_METHOD'] == 'DELETE' || (isset($_POST['_method']) && strcasecmp($_POST['_method'], 'DELETE') == 0)) ? route($path, $func) : false; }
+function __web_php_init() {
+    $base = parse_url($_SERVER['SCRIPT_NAME'], PHP_URL_PATH);
+    $base = substr($base, 0, strrpos($base, '/')) . '/';
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $path = substr($path, 0, strrpos($path, '/')) . '/';
+    $full = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+    $full .= $_SERVER['HTTP_HOST'];
+    $port = $_SERVER['SERVER_PORT'];
+    if (($full[4] === 's' &&  $port !== '443') || $port !== '80') $full .= ":$port";
+    define('WEB_URL_ROOT', $full . '/');
+    define('WEB_URL_PATH', $path);
+    define('WEB_URL_BASE', $base);
+    register_shutdown_function(function() {
+        if (!defined('SID') || !isset($_SESSION['web.php:flash'])) return;
+        $flash =& $_SESSION['web.php:flash'];
+        foreach($flash as $key => $hops) {
+            if ($hops === 0) unset($_SESSION[$key], $flash[$key]); else $flash[$key]--;
+        }
+        if (count($flash) === 0) unset($flash);
+    });
+}
+function get($path, $func = null) { return $_SERVER['REQUEST_METHOD'] == 'GET' ? route($path, $func) : false; }
+function post($path, $func = null) { return $_SERVER['REQUEST_METHOD'] == 'POST' ? route($path, $func) : false; }
+function put($path, $func = null) { return $_SERVER['REQUEST_METHOD'] == 'PUT' || (isset($_POST['_method']) && strcasecmp($_POST['_method'], 'PUT') === 0) ? route($path, $func) : false; }
+function delete($path, $func = null) { return $_SERVER['REQUEST_METHOD'] == 'DELETE' || (isset($_POST['_method']) && strcasecmp($_POST['_method'], 'DELETE') === 0) ? route($path, $func) : false; }
 function route($path, $func = null) {
     static $matched = false;
     if ($matched) return false;
     $matched = $func == null;
     if ($matched) return run($path);
-    $subject = trim(substr(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), strlen(substr($_SERVER['SCRIPT_NAME'], 0, -9))), '/');
+    $subject = trim(substr(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), strlen(WEB_URL_BASE)), '/');
     $params = array();
     if (stripos($path, 'r/') === 0) {
         $pattern = substr($path, 1);
@@ -45,41 +66,59 @@ function run($func, $params = array()) {
             $ctrl = array(new $clazz, $method);
         }
     }
-    if (is_callable($ctrl)) {
-        return $ctrl($params);
-    }
+    if (is_callable($ctrl)) return $ctrl($params);
     trigger_error("Invalid function or method $func.", E_USER_WARNING);
 }
-function redirect($url = null, $code = 302) {
-    if (headers_sent($file, $line)) return trigger_error("Headers already sent in $file on line $line.", E_USER_ERROR);
-    if ($code == 301) header($_SERVER['SERVER_PROTOCOL'] ?: 'HTTP/1.0' . ' 301 Moved Permanently');
-    header('Location: ' . url($url));
+function url($url, $abs = false) {
+    if (parse_url($url, PHP_URL_SCHEME) !== null) return $url;
+    if (strpos($url, '~/') === 0) {
+        $url = WEB_URL_BASE . substr($url, 2);
+    } elseif (strpos($url, '/') !== 0) {
+        $url = WEB_URL_PATH . $url;
+    }
+    $parts = explode('/', $url);
+    $url = array();
+    for ($i=0; $i < count($parts); $i++) {
+        if ($parts[$i] === '' || $parts[$i] === '.') continue;
+        if ($parts[$i] === '..') {
+            array_pop($url);
+            continue;
+        }
+        array_push($url, $parts[$i]);
+    }
+    $url = implode('/', $url);
+    return ($abs) ? WEB_URL_ROOT . $url : '/' . $url;
 }
-function url($url = null) {
-    if ($url != null) extract(parse_url($url));
-    if (!isset($port) && isset($scheme)) $port = $scheme == 'http' ? 80 : 443;
-    if (!isset($scheme)) $scheme = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
-    if (!isset($host)) $host = $_SERVER['HTTP_HOST'];
-    if (!isset($port)) $port = $_SERVER['SERVER_PORT'];
-    if (!isset($path)) $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    if (strpos($path, '~') === 0) $path = substr($_SERVER['SCRIPT_NAME'], 0, -9) . ltrim(substr($path, 1), '/');
-    if (strpos($path, '/') !== 0) $path = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') . '/' . ltrim($path, '/');
-    if (!isset($query) && $url == null) $query = $_SERVER['QUERY_STRING'];
-    $url = $scheme . '://' . $host;
-    if (($scheme == 'http' && $port != 80) || ($scheme == 'https' && $port != 443)) $url .= ':' . $port;
-    $url .= $path;
-    if (isset($query)) $url .= '?' . $query;
-    if (isset($fragment)) $url .= '#' . $fragment;
-    return $url;
+function slug($title, $replace = null, $delimiter = '-') {
+	if ($replace !== null) {
+            $title = strtr($title, $replace, str_repeat(' ', strlen($title)));
+	}
+	$title = iconv('UTF-8', 'ASCII//TRANSLIT', $title);
+	$title = preg_replace('#[^a-z0-9/_|+\s-]#i', '', $title);
+	$title = strtolower(trim($title, '/_|+ -'));
+	$title = preg_replace('#[/_|+\s-]+#', $delimiter, $title);
+	return $title;
+}
+function redirect($url = null, $code = 301) {
+    if (headers_sent($file, $line)) {
+        trigger_error("Headers already sent in $file on line $line.", E_USER_ERROR);
+    } else {
+        header('Location: ' . url($url, true), true, $code);
+    }
+    die;
 }
 function session_ensure($regenerate = false) {
     static $started;
     if ($started) return;
-    if (headers_sent($file, $line) && (!defined('SID') || $regenerate)) return trigger_error("Headers already sent in $file on line $line.", E_USER_ERROR);
+    if (headers_sent($file, $line) && (!defined('SID') || $regenerate)) {
+        trigger_error("Headers already sent in $file on line $line.", E_USER_ERROR);
+        return;
+    }
     if (!defined('SID')) session_start();
     if (isset($_SESSION['web.php:session'])) {
         if ($_SESSION['web.php:session'] !== crc32($_SERVER['HTTP_USER_AGENT'])) {
-            return trigger_error('Possible Session Hijacking Attempt.', E_USER_ERROR);
+            trigger_error('Possible Session Hijacking Attempt.', E_USER_ERROR);
+            return;
         }
     } else {
         $_SESSION['web.php:session'] = crc32($_SERVER['HTTP_USER_AGENT']);
@@ -89,23 +128,18 @@ function session_ensure($regenerate = false) {
 function flash($name, $value, $hops = 1) {
     session_ensure();
     $_SESSION[$name] = $value;
-    $_SESSION["web.php:flash:$name"] = $hops;
-}
-function block($name, $operation = 'render') {
-    static $block;
-    static $blocks = array();
-    if ($name !== null) $block = $name;
-    switch ($operation) {
-        case 'start': ob_start(); break;
-        case 'end': $blocks[$block] = ob_get_clean(); break;
-        case 'render': echo $blocks[$block];
+    if (!isset($_SESSION['web.php:flash'])) {
+        $_SESSION['web.php:flash'] = array($name => $hops);
+    } else {
+        $_SESSION['web.php:flash'][$name] = $hops;
     }
 }
-function block_start($name) {
-    block($name, 'start');
-}
-function block_end() {
-    block(null, 'end');
+function block(&$block = false) {
+    if ($block === false) {
+        ob_end_clean();
+    } else {
+        ob_start(function($buffer) use (&$block) { return $block = $buffer; });
+    }
 }
 class view {
     function __construct($file) {
@@ -123,81 +157,4 @@ class view {
         } while (true);
     }
 }
-class form extends ArrayObject {
-    private $filters;
-    private $value;
-    function __construct($args = null) {
-        if ($args == null) return;
-        foreach ($args as $name => $value) {
-            if (is_array($value) && !isset($value[0][0])) {
-                $this[$name] = new self($value);
-            } else {
-                $this[$name] = new self;
-                $this[$name]->value = $value;
-            }
-        }
-    }
-    function __set($name, $value) {
-        if ($name == 'filters') {
-            $this->filters =  is_string($value)? explode(',', $value) : (array) $value;
-            return;
-        }
-        if (!isset($this[$name])) $this[$name] = new self;
-        $this[$name]['value'] = $value;
-    }
-    function __get($name) {
-        if (!isset($this[$name])) $this[$name] = new self;
-        $field = $this[$name];
-        return $field;
-    }
-    function __call($name, $args) {
-        if (!isset($args[0])) return print $this->value ?: '';
-        return print $this->value ?: $args[0];
-    }
-    function __toString() {
-        return $this->value ?: '';
-    }
-    function validate() {
-        if ($this->filters == null) return true;
-        $validates = true;
-        foreach ($this->filters as $filter) {
-            switch (trim($filter)) {
-                // filters
-                case 'trim':  $this->value = trim($this->value); break;
-                case 'ltrim': $this->value = ltrim($this->value); break;
-                case 'rtrim':
-                case 'chop':  $this->value = rtrim($this->value); break;
-                // validators
-                case 'req':   $validates = strlen($this->value) > 0; break;
-                case 'bool':  $validates = false !== filter_var($this->value, FILTER_VALIDATE_BOOLEAN); break;
-                case 'int':   $validates = false !== filter_var($this->value, FILTER_VALIDATE_INT); break;
-                case 'float': $validates = false !== filter_var($this->value, FILTER_VALIDATE_FLOAT); break;
-                case 'ip':    $validates = false !== filter_var($this->value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6); break;
-                case 'ipv4':  $validates = false !== filter_var($this->value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4); break;
-                case 'ipv6':  $validates = false !== filter_var($this->value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6); break;
-                case 'email': $validates = false !== filter_var($this->value, FILTER_VALIDATE_EMAIL); break;
-                case 'url':   $validates = false !== filter_var($this->value, FILTER_VALIDATE_URL); break;
-                default:
-                    // regex validator
-                    if ((strpos($filter, '/')) === 0) {
-                        $validates = preg_match($filter, $this->value);
-                    }
-                    break;
-            }
-            if (!$validates) return false;
-        }
-        return true;
-    }
-}
-register_shutdown_function(function() {
-    if (!defined('SID')) return;
-    $keys = array_keys($_SESSION);
-    foreach($keys as $key) {
-        if (strpos($key, 'web.php:flash:') === 0) {
-            if ($_SESSION[$key] === 0)
-                unset($_SESSION[$key], $_SESSION[substr($key, 14)]);
-            else
-                $_SESSION[$key]--;
-        }
-    }
-});
+__web_php_init();
