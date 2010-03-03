c8120642fbcd36536d43661095c02c64943c7d8e
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-03-03 23:02:03 +0000

    Added title function and simplified slug.

--- examples/hello-world/index.php
+++ examples/hello-world/index.php
@@ -29,7 +29,7 @@ require 'web.php';
 
 get('/', function ($params) {
     $view = new view('views/index.phtml');
-    $view->header = 'Lorem Ipsum ';
+    $view->header = 'Lorem Ipsum';
     $view->body = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     echo $view;
 });
