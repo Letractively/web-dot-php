1aa5c13173eb48481c4771f2597a6eec7d551967
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-06-01 21:19:05 +0000

    Datetime fixes

--- examples/phpbetz/lib/web.php
+++ examples/phpbetz/lib/web.php
@@ -114,6 +114,56 @@ namespace {
     function forward($url, $exit = true) {
         web\dispatch($url, $exit);
     }
+    function status($code) {
+        switch ($code) {
+            case 100: $msg = 'Continue'; break;
+            case 200: $msg = 'OK'; break;
+            case 201: $msg = 'Created'; break;
+            case 101: $msg = 'Switching Protocols'; break;
+            case 200: $msg = 'OK'; break;
+            case 201: $msg = 'Created'; break;
+            case 202: $msg = 'Accepted'; break;
+            case 203: $msg = 'Non-Authoritative Information'; break;
+            case 204: $msg = 'No Content'; break;
+            case 205: $msg = 'Reset Content'; break;
+            case 206: $msg = 'Partial Content'; break;
+            case 300: $msg = 'Multiple Choices'; break;
+            case 301: $msg = 'Moved Permanently'; break;
+            case 302: $msg = 'Found'; break;
+            case 303: $msg = 'See Other'; break;
+            case 304: $msg = 'Not Modified'; break;
+            case 305: $msg = 'Use Proxy'; break;
+            case 306: $msg = '(Unused)'; break;
+            case 307: $msg = 'Temporary Redirect'; break;
+            case 400: $msg = 'Bad Request'; break;
+            case 401: $msg = 'Unauthorized'; break;
+            case 402: $msg = 'Payment Required'; break;
+            case 403: $msg = 'Forbidden'; break;
+            case 404: $msg = 'Not Found'; break;
+            case 405: $msg = 'Method Not Allowed'; break;
+            case 406: $msg = 'Not Acceptable'; break;
+            case 407: $msg = 'Proxy Authentication Required'; break;
+            case 408: $msg = 'Request Timeout'; break;
+            case 409: $msg = 'Conflict'; break;
+            case 410: $msg = 'Gone'; break;
+            case 411: $msg = 'Length Required'; break;
+            case 412: $msg = 'Precondition Failed'; break;
+            case 413: $msg = 'Request Entity Too Large'; break;
+            case 414: $msg = 'Request-URI Too Long'; break;
+            case 415: $msg = 'Unsupported Media Type'; break;
+            case 416: $msg = 'Requested Range Not Satisfiable'; break;
+            case 417: $msg = 'Expectation Failed'; break;
+            case 500: $msg = 'Internal Server Error'; break;
+            case 501: $msg = 'Not Implemented'; break;
+            case 502: $msg = 'Bad Gateway'; break;
+            case 503: $msg = 'Service Unavailable'; break;
+            case 504: $msg = 'Gateway Timeout'; break;
+            case 505: $msg = 'HTTP Version Not Supported'; break;
+            default: return;
+        }
+        $protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1';
+        header(trim(sprintf('%s %u %s', $protocol, $code, $msg)));
+    }
     function redirect($url, $code = 301, $exit = true) {
         header('Location: ' . url($url, true), true, $code);
         if ($exit) exit;
