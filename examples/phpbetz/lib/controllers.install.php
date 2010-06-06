54bd2390a3f32879331ea37df453761d467c5535
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-06-06 23:33:16 +0000

    install mode

--- examples/phpbetz/lib/controllers.install.php
+++ examples/phpbetz/lib/controllers.install.php
@@ -1,24 +1,12 @@
 <?php
-
 get('/install', function() {
     db\install\tables();
+    db\install\triggers();
     db\install\views();
     db\install\teams();
     db\install\games();
 });
 
-get('/install/views', function() {
-    db\install\views();
-});
-
 get('/install/admins', function() {
     db\install\admins();
-});
-
-get('/install/draw', function() {
-    db\install\add_draw_to_games();
-});
-
-get('/install/paid', function() {
-    db\install\add_paid_to_users();
 });
