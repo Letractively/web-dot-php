27fa7483dc78daa9263fd24e3a9b00d3bb12d204
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-06-06 23:53:51 +0000

    secure mode

--- examples/phpbetz/index.php
+++ examples/phpbetz/index.php
@@ -5,10 +5,31 @@ define('starttime', microtime(true));
 define('database', 'data/worldcup2010.sq3');
 define('secret', 'Replace this on a production server.');
 define('ajax', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
-define('install', false);
 define('DATE_SQLITE', 'Y-m-d\TH:i:s');
 define('LOG_PATH', 'data');
 define('LOG_LEVEL', LOG_WARNING);
+
+set_exception_handler(function(Exception $ex) {
+    @error(sprintf('%s [%s:%s]', $ex->getMessage(), $ex->getFile(), $ex->getLine()));
+    while(@ob_end_clean());
+    if (ajax) {
+        status(500);
+        exit;
+    } else {
+        @forward('/error');
+    }
+});
+
+set_error_handler(function($code, $message, $file, $line, $context) {
+    @error(sprintf('%s [%s:%s]', $message, $file, $line));
+    while(@ob_end_clean());
+    if (ajax) {
+        status(500);
+        exit;
+    } else {
+        @forward('/error');
+    }
+});
 
 date_default_timezone_set('Europe/Helsinki');
 setlocale(LC_ALL, 'fi_FI.utf8');
@@ -17,48 +38,17 @@ session_start();
 require 'lib/utils.php';
 require 'lib/web.php';
 require 'lib/db.php';
+require 'lib/controllers.login.php';
 
-if (install) {
-    require 'lib/db.install.php';
-    require 'lib/controllers.install.php';
-} else {
-    require 'lib/controllers.login.php';
+authenticate();
 
-    set_exception_handler(function(Exception $ex) {
-        @error(sprintf('%s [%s:%s]', $ex->getMessage(), $ex->getFile(), $ex->getLine()));
-        while(@ob_end_clean());
-        if (ajax) {
-            status(500);
-            exit;
-        } else {
-            @forward('/error');
-        }
-    });
-
-    set_error_handler(function($code, $message, $file, $line, $context) {
-        @error(sprintf('%s [%s:%s]', $message, $file, $line));
-        while(@ob_end_clean());
-        if (ajax) {
-            status(500);
-            exit;
-        } else {
-            @forward('/error');
-        }
-    });
-
-    authenticate();
-
-    if (authenticated) {
-        require 'lib/controllers.main.php';
-        require 'lib/controllers.bets.php';
-        require 'lib/controllers.stats.php';
-    }
-
-    if (admin) {
-        require 'lib/controllers.admin.php';
-    }
-    
-    portlets();
+if (authenticated) {
+    require 'lib/controllers.main.php';
+    require 'lib/controllers.bets.php';
+    require 'lib/controllers.stats.php';
 }
 
+if (admin) require 'lib/controllers.admin.php';
+
+portlets();
 dispatch();
