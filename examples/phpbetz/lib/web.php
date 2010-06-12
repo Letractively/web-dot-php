d9634283f8839ae71d3cdaa623f5929ad0532906
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-06-12 17:03:30 +0000

    changed chat to have links in new window.

--- examples/phpbetz/lib/web.php
+++ examples/phpbetz/lib/web.php
@@ -311,18 +311,6 @@ namespace {
             return htmlspecialchars($value, $quote, $charset, $double);
         };
     }
-    function links($value) {
-        if($value == '' || !preg_match('/(http|www\.|@)/i', $value)) return $value;
-        $lines = explode("\n", $value); $value = '';
-        while (list($k, $l) = each($lines)) {
-            $l = preg_replace("/([ \t]|^)www\./i", "\\1http://www.", $l);
-            $l = preg_replace("/(http:\/\/[^ )\r\n!]+)/i", "<a href=\"\\1\">\\1</a>", $l);
-            $l = preg_replace("/(https:\/\/[^ )\r\n!]+)/i", "<a href=\"\\1\">\\1</a>", $l);
-            $l = preg_replace("/([-a-z0-9_]+(\.[_a-z0-9-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)+))/i", "<a href=\"mailto:\\1\">\\1</a>", $l);
-            $value .= $l."\n";
-        }
-        return $value;
-    }
     function checkbox(&$value) {
         $value = isset($value);
         return true;
