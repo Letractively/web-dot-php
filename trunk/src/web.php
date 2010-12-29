e6fa71ab6ebd84e9acd57f4bdc5e1c90e7865d0b
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2010-12-29 22:33:17 +0000

    Added password hashing functions.

--- trunk/src/web.php
+++ trunk/src/web.php
@@ -321,3 +321,39 @@ namespace log {
         $messages .= sprintf('%s %7s %-20s %s', date('Y-m-d H:i:s.', $sec) . substr($usec, 2, 3) , level($level), basename($trace[1]['file']) . ':' . $trace[1]['line'], trim($message) . PHP_EOL);
     }
 }
+// Password (PHP >= 5.3 version of http://www.openwall.com/phpass/)
+namespace password {
+    function hash($password, $iterations = 8)
+    {
+        $random = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+        $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        if ($iterations < 4 || $iterations > 31) $iterations = 8;
+        $salt = '$2a$';
+        $salt .= chr(ord('0') + $iterations / 10);
+        $salt .= chr(ord('0') + $iterations % 10);
+        $salt .= '$';
+        $i = 0;
+        do {
+            $c1 = ord($random[$i++]);
+            $salt .= $itoa64[$c1 >> 2];
+            $c1 = ($c1 & 0x03) << 4;
+            if ($i >= 16) {
+                $salt .= $itoa64[$c1];
+                break;
+            }
+            $c2 = ord($random[$i++]);
+            $c1 |= $c2 >> 4;
+            $salt .= $itoa64[$c1];
+            $c1 = ($c2 & 0x0f) << 2;
+            $c2 = ord($random[$i++]);
+            $c1 |= $c2 >> 6;
+            $salt .= $itoa64[$c1];
+            $salt .= $itoa64[$c2 & 0x3f];
+        } while (true);
+        $hash = crypt($password, $salt);
+        return strlen($hash) == 60 ? $hash : '*';
+    }
+    function check($password, $hash) {
+        return crypt($password, $hash) == $hash;
+    }
+}
