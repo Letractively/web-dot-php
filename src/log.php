8a94ceac3b0f9e3fa84b6ff7051f84b2214e4f69
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2011-09-24 17:29:04 +0000

    Improved logging.

--- src/log.php
+++ src/log.php
@@ -1,29 +1,36 @@
 <?php
 // Logging
 namespace log {
-    function debug($message) { append($message, LOG_DEBUG); }
-    function info($message) { append($message, LOG_INFO); }
-    function warn($message) { append($message, LOG_WARNING); }
-    function error($message) { append($message, LOG_ERR); }
-    function write($message, $level) { append($message, $level); }
-    function level($level) {
-        if ($level > LOG_INFO) return 'DEBUG';
-        if ($level > LOG_WARNING) return 'INFO';
-        return $level > LOG_ERR ? 'WARNING' : 'ERROR';
-    }
-    function append($message, $level) {
-        defined('LOG_PATH')  or define('LOG_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/data');
-        defined('LOG_LEVEL') or define('LOG_LEVEL', LOG_DEBUG);
-        defined('LOG_FILE')  or define('LOG_FILE', 'Y-m-d.\l\o\g');
-        if (LOG_LEVEL < $level) return;
-        static $messages = null;
-        if ($messages == null) {
-            register_shutdown_function(function() use (&$messages) {
-                file_put_contents(rtrim(LOG_PATH, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . date_create()->format(LOG_FILE), $messages, FILE_APPEND | LOCK_EX);
-            });
-        }
-        $trace = debug_backtrace();
-        list($usec, $sec) = explode(' ', microtime());
-        $messages .= sprintf('%s %7s %-20s %s', date('Y-m-d H:i:s.', $sec) . substr($usec, 2, 3) , level($level), basename($trace[1]['file']) . ':' . $trace[1]['line'], trim($message) . PHP_EOL);
-    }
+	function appenders() {
+		static $appenders = array();
+		if (func_num_args() == 0) return $appenders;
+		$args = func_get_args();
+		foreach ($args as $arg) $appenders[] = $arg;
+	}
+	function debug($message) { append($message, LOG_DEBUG, debug_backtrace()); }
+	function info($message) { append($message, LOG_INFO, debug_backtrace()); }
+	function warn($message) { append($message, LOG_WARNING, debug_backtrace()); }
+	function error($message) { append($message, LOG_ERR, debug_backtrace()); }
+	function append($message, $level, $trace) {
+		$appenders = appenders();
+		foreach ($appenders as $appender) $appender($message, $level, $trace);
+	}
+	function level($level) {
+		if ($level > LOG_INFO) return 'DEBUG';
+		if ($level > LOG_WARNING) return 'INFO';
+		return $level > LOG_ERR ? 'WARNING' : 'ERROR';
+	}
+	function file($file, $log_level = LOG_DEBUG) {
+		return function($message, $level, $trace) use ($file, $log_level) {
+			if ($log_level < $level) return;
+			static $messages = null;
+			if ($messages == null) {
+				register_shutdown_function(function() use ($file, &$messages) {
+					file_put_contents($file, $messages, FILE_APPEND | LOCK_EX);
+				});
+			}
+			list($usec, $sec) = explode(' ', microtime());
+			$messages .= sprintf('%s %7s %-20s %s', date('Y-m-d H:i:s.', $sec) . substr($usec, 2, 3) , level($level), basename($trace[0]['file']) . ':' . $trace[0]['line'], trim($message) . PHP_EOL);
+		};
+	}	
 }
