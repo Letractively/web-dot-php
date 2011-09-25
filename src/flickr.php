41ff303af5e84cc0ce4f44504743c83fd5a55eab
aapo.laakkonen <aapo.laakkonen@61f49d13-c23c-0410-9264-81a12cdd8f14>
2011-09-25 11:17:08 +0000

    spaces to tabs + form implementation modified.

--- src/flickr.php
+++ src/flickr.php
@@ -2,18 +2,18 @@
 // Flickr
 // TODO: POSTing to Flickr not implemented.
 function flickr($args) {
-    $endpoint = isset($args['endpoint']) ? $args['endpoint'] : 'http://api.flickr.com/services/rest/';
-    $secret = isset($args['api_secret']) ? $args['api_secret'] : null;
-    unset($args['endpoint'], $args['api_secret']);
-    if ($secret != null) {
-        ksort($args);
-        $api_sig = $secret;
-        foreach($args as $k => $v) $api_sig .= $k . $v;
-        $api_sig = md5($api_sig);
-        $args['api_sig'] = $api_sig;
-    }
-    $url = $endpoint . '?' . http_build_query($args);
-    if (substr($endpoint, -15) === '/services/auth/') return $url;
-    $response = file_get_contents($url);
-    return isset($args['format']) && $args['format'] === 'php_serial' ? unserialize($response) : $response;
+	$endpoint = isset($args['endpoint']) ? $args['endpoint'] : 'http://api.flickr.com/services/rest/';
+	$secret = isset($args['api_secret']) ? $args['api_secret'] : null;
+	unset($args['endpoint'], $args['api_secret']);
+	if ($secret != null) {
+		ksort($args);
+		$api_sig = $secret;
+		foreach($args as $k => $v) $api_sig .= $k . $v;
+		$api_sig = md5($api_sig);
+		$args['api_sig'] = $api_sig;
+	}
+	$url = $endpoint . '?' . http_build_query($args);
+	if (substr($endpoint, -15) === '/services/auth/') return $url;
+	$response = file_get_contents($url);
+	return isset($args['format']) && $args['format'] === 'php_serial' ? unserialize($response) : $response;
 }
